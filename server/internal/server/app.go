9e21175bed5acef73a4bd949b4e9e92da5474ec4
owenhochwald <owenhochwald@gmail.com>
2025-10-02 23:40:54 -0700

    add skeleton init for app struct

--- server/internal/server/app.go
+++ server/internal/server/app.go
@@ -1,1 +1,17 @@
 package server
+
+import "github.com/owenHochwald/bathroomQueueScheduler/internal/services"
+
+type Application struct {
+	BathroomService services.BathroomServiceInterface
+}
+
+func NewApplication() *Application {
+	application := &Application{}
+	initServices(application)
+	return application
+}
+
+func initServices(app *Application) {
+	app.BathroomService = services.NewBathroomService()
+}
