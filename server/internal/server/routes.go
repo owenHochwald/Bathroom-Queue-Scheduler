c5bad1188aeb7a649f5f18610712f6083931c2ec
owenhochwald <owenhochwald@gmail.com>
2025-10-02 23:41:04 -0700

    setup additional rest api routes

--- server/internal/server/routes.go
+++ server/internal/server/routes.go
@@ -6,14 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine) {
+func SetupRoutes(r *gin.Engine, app *Application) {
 	api := r.Group("/api")
-	api.POST("/queue/join")
-	api.GET("/queue/status")
-	api.POST("bathroom/occupy")
-	api.POST("bathroom/vacate")
+
+	queue := api.Group("/queue")
+	queue.POST("/join")
+	queue.DELETE("/leave/:user_id")
+
+	bathroom := api.Group("/bathroom")
+	bathroom.POST("/occupy")
+	bathroom.POST("/vacate")
+
+	api.GET("/status")
+	api.GET("/stats/:user_id")
+	api.GET("/leaderboards")
 
 	r.GET("/ws")
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(
 			http.StatusOK,
