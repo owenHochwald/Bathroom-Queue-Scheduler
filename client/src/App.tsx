df5104ef5c66caeb0b00ad359739580999ea91f1
owenhochwald <owenhochwald@gmail.com>
2025-10-06 17:57:32 -0700

    add axios and tanstack query provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+            </div>
+        </QueryClientProvider>
+
+    );
 }
 
 export default App;
