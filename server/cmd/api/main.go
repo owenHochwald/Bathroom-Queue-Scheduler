b620d2caea0c6b5f97b610c7a4b95674a994ad89
owenhochwald <owenhochwald@gmail.com>
2025-10-06 19:19:20 -0700

    add cors

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 
+    "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/owenHochwald/bathroomQueueScheduler/internal/config"
 	"github.com/owenHochwald/bathroomQueueScheduler/internal/server"
@@ -10,6 +11,14 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	db, err := config.ConnectDB()
 
